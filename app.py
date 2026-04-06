5d3a0744f5c5b11fb8d8ab487e9f6a091a8c11bf
AyushYadav82 <ayushyadav190206@gmail.com>
2026-04-07 00:06:03 +0530

    this is my first change

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def add_expense():
         try:
             amount = float(request.form.get('amount'))
         except:
-            return "❌ Invalid amount"
+            return "❌ Invald amount"
 
         note = request.form.get('note')
 
